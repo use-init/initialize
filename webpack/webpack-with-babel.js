78cdbc3eb086e0df1027a9201eb03786968df8d9
Hans Christian Reinl <info@drublic.de>
2017-10-17 17:15:13 +0200

    Initialize

---
+++ webpack/webpack-with-babel.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const DEBUG = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    'filename': [
+      path.resolve(__dirname, 'filename.js')
+    ]
+  },
+  devtool: DEBUG ? 'inline-sourcemap' : false,
+  cache: true,
+  output: {
+    path: path.resolve(__dirname, 'libs/'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: []
+}
+
+if (!DEBUG) {
+  module.exports.output.filename = '[name].min.js';
+
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
